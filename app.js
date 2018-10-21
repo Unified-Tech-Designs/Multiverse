0c3641057ce1eb5a72c4b87e4094b7a5b3a22033
Warsame <warsame277@outlook.com>
2018-10-21 15:21:05 +0300

    server setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const path = require("path")
+
+
+
+
+const app = express()
+
+const port = 5000 || env.port
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => res.sendFile("index"))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
